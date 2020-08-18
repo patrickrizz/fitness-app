8b38793eb32e8469fdee66c236b5a4b61281d663
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-08-18 00:58:17 -0400

    added dashboard route

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config({ silent: true })
+require('./initializers/googleAuth')
+const flash = require('express-flash')
 const express = require('express')
 const path = require('path');
-const middleware = require("./middleware/middleware")
-const app = express()
+const app = express();
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
 
 let indexRouter = require('./routes/indexRoute')
+let authRouter = require('./routes/authRoute')
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -17,16 +21,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')))
 
+//express session
+app.use(session({
+    secret: [process.env.SESSION_SECRET, process.env.SESSION_SECRET_2],
+    name: 'awesomeSession',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60000
+    } //only used with https, can't with http. It secures the cookie, sameSite is for same site enforcment
+}));
 
-//init middleware
-middleware()
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//express flash
+app.use(flash())
+
+//global variables and remove headers
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    res.removeHeader('X-Powered-By')
+    res.removeHeader('Server')
+    next()
+})
 
 //routes
 app.use('/', indexRouter)
-//app.use('/auth', require('./auth'))
+app.use('/auth', authRouter)
 
 //404
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
     res.status(404).send('<h1 style="text-align: center font-size: 3.5em">404 Reqeust Not Found</h1>')
 })
 
